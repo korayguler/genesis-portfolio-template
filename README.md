bf2544dcd75774844b4412d176deaac2bde166f0
korayguler <tr.koray.guler@gmail.com>
2020-10-28 00:29:01 +0300

    :rocket: initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+### genesis-portfolio-template
+
+### How to run
+
+```
+git clone https://github.com/korayguler/genesis-portfolio-template
+```
+
+```
+cd genesis-portfolio-template
+```
+
+```
+npm install
+```
+
+```
+npm start
+```
---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browserSync = require('browser-sync');
+
+gulp.task('sass', () => {
+  return gulp
+    .src('sass/app.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('watch', () => {
+  browserSync.init({
+    watch: true,
+    server: './',
+  });
+
+  gulp.watch('sass/**/*.scss', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series('watch'));
