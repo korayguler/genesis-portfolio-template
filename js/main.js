9fad9ba82d24bbb3be8fca0e1fb0d1e34e5078ec
korayguler <tr.koray.guler@gmail.com>
2020-10-28 19:14:30 +0300

    ⚡️

--- js/main.js
+++ js/main.js
@@ -3,5 +3,11 @@
 const menu = document.querySelector('#menu');
 const nav = document.querySelector('#nav');
 menu.addEventListener('click', (e) => {
-  nav.classList.toggle('show');
+  if (nav.classList[1] === 'show') {
+    nav.classList.remove('show');
+    menu.innerHTML = '<i class="fas fa-bars"></i>';
+  } else {
+    nav.classList.add('show');
+    menu.innerHTML = '<i class="fas fa-times"></i>';
+  }
 });
