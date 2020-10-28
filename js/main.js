4532fa51b6765bded94198772b3832712535c928
korayguler <tr.koray.guler@gmail.com>
2020-10-28 16:11:51 +0300

    :sparkles: mobile compatibility, refactoring

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
 //your main.js
+
+const menu = document.querySelector('#menu');
+const nav = document.querySelector('#nav');
+menu.addEventListener('click', (e) => {
+  nav.classList.toggle('show');
+});
